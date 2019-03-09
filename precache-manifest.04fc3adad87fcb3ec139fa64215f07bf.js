256dd67dbf383ad50d565bb13ba69240e69b6f2f
Mostafa-ZR <48276039+MostafaZr-Dev@users.noreply.github.com>
2019-03-09 12:22:32 +0330

    Updates

---
+++ precache-manifest.04fc3adad87fcb3ec139fa64215f07bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8d9c99a4f19116c6d931",
+    "url": "/weather-app/static/js/runtime~main.02d38d5d.js"
+  },
+  {
+    "revision": "02b2d36276b08d7a014e",
+    "url": "/weather-app/static/js/main.a94c59a3.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/weather-app/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "02b2d36276b08d7a014e",
+    "url": "/weather-app/static/css/main.dc5d408c.chunk.css"
+  },
+  {
+    "revision": "8d938a8581886909ee0fdc9520955be2",
+    "url": "/weather-app/index.html"
+  }
+];
